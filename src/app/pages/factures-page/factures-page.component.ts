1a59c2a5733346830a00c9f70e7f2c950f5d9026
kratos <desmond8500@gmail.com>
2025-02-21 08:37:22 +0000

    mise à jour des factures

--- src/app/pages/factures-page/factures-page.component.ts
+++ src/app/pages/factures-page/factures-page.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, resource } from '@angular/core';
 
 @Component({
   selector: 'app-factures-page',
   imports: [],
   templateUrl: './factures-page.component.html',
-  styleUrl: './factures-page.component.scss'
+  styleUrl: './factures-page.component.scss',
 })
 export class FacturesPageComponent {
-
+  server = 'https://dash3.yonkou.info/api/v1';
+  factures = resource({
+    loader: () => fetch(this.server+'/factures').then((response) => response.json()),
+  });
 }
